d55d0d76bf13caff2d245ece188d24fce701674a
kwcn <1049252607@qq.com>
2018-03-22 00:40:18 +0800

    20180322

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="d63478fd-89e8-48bf-affd-810e60fe1e70")
+public class DataBindingInfo {}
